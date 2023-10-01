67c7443952118cc97b229888ddffb6811ba22d42
Rodrigo <velez59@gmail.com>
2023-10-01 14:13:07 -0700

    feat: drew background and first character to canvas

--- js/app.js
+++ js/app.js
@@ -2,16 +2,40 @@
 const GameViewport = {
     width: 384,
     height: 224,
-    scale: 4
 }
 
 // Ensure page loads before code runs
-window.onload = function() {
+window.onload = function () {
     const canvas = document.querySelector('canvas');
     const ctx = canvas.getContext('2d');
 
-    canvas.style.width = `${GameViewport.width * GameViewport.scale}px`;
-    canvas.style.height = `${GameViewport.height * GameViewport.scale}px`;
+    canvas.width = GameViewport.width;
+    canvas.height = GameViewport.height;
 
-    console.log(ctx);
+    const [ryu, background] = document.querySelectorAll('img');
+    const position = {
+        x: GameViewport.width / 2 - ryu.width / 2,
+        y: 110
+    };
+
+    let velocity = 1;
+
+    // animation function
+    function frame() {
+        position.x += velocity;
+
+        // check for x axis boundaries
+        if (position.x > GameViewport.width - ryu.width || position.x < 0) {
+            velocity = -velocity;
+        }
+
+        ctx.drawImage(background, 0, 0);
+        ctx.drawImage(ryu, position.x, position.y);
+
+        window.requestAnimationFrame(frame);
+    }
+
+    //requests animation
+    window.requestAnimationFrame(frame);
+
 }
