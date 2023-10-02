221de9d0c260e9afb832a89579cf94a048f4a40b
Rodrigo <velez59@gmail.com>
2023-10-02 13:05:22 -0700

    chore: added figher state constants to fighter animations

--- js/constants/figther.js
+++ js/constants/figther.js
@@ -2,3 +2,8 @@ export const FighterDirection = {
     LEFT: -1,
     RIGHT: 1,
 };
+
+export const FighterState = {
+    WALK_FORWARD: 'walkForwards',
+    WALK_BACKWARD: 'walkBackwards',
+}
--- js/entities/characters/Ken.js
+++ js/entities/characters/Ken.js
@@ -1,3 +1,4 @@
+import { FighterState } from "../../constants/figther.js";
 import { Character } from "./Fighters.js";
 
 export class Ken extends Character {
@@ -25,8 +26,8 @@ export class Ken extends Character {
         ]);
 
         this.animations = {
-            'walkForwards': ['forwards-1', 'forwards-2', 'forwards-3', 'forwards-4', 'forwards-5', 'forwards-6'],
-            'walkBackwards': ['backwards-1', 'backwards-2', 'backwards-3', 'backwards-4', 'backwards-5', 'backwards-6'],
+            [FighterState.WALK_FORWARD]: ['forwards-1', 'forwards-2', 'forwards-3', 'forwards-4', 'forwards-5', 'forwards-6'],
+            [FighterState.WALK_BACKWARD]: ['backwards-1', 'backwards-2', 'backwards-3', 'backwards-4', 'backwards-5', 'backwards-6'],
         }
     }
 }
