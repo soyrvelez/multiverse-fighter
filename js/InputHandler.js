9fe7135d94f99022f2461b3fe998ff098da1a930
Rodrigo <velez59@gmail.com>
2023-10-03 08:54:18 -0700

    chore: added event listeners for keydown and keyup and heldkeys tracker in InputHandler

--- js/InputHandler.js
+++ js/InputHandler.js
@@ -1,5 +1,21 @@
-export function registerKeyboardEvents() {
-    window.addEventListener('keydown', (e) => {
-        console.log(e);
-    })
+const heldKeys = new Set();
+
+function handleKeyDown(event) {
+    event.preventDefault();
+
+    heldKeys.add(event.code);
 }
+
+function handleKeyUp(event) {
+    event.preventDefault();
+
+    heldKeys.delete(event.code);
+}
+
+export function registerKeyboardEvents() {
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keyup', handleKeyDown);
+}
+
+export const isKeyDown = (code) => heldKeys.has(code);
+export const isKeyUp = (code) => !heldKeys.has(code);
