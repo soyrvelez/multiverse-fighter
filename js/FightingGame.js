0e7e63b158fec48d5953621adf0498d4675525d8
Rodrigo <velez59@gmail.com>
2023-10-02 21:04:26 -0700

    feat: refactored game into a separate class in FightingGame.js

---
+++ js/FightingGame.js
@@ -0,0 +1,83 @@
+import { Ryu } from './entities/characters/Ryu.js';
+import { Stage } from './entities/Background.js';
+import { FpsCounter } from './entities/FpsCounter.js';
+import { STAGE_FLOOR } from './constants/stage.js';
+import { Ken } from './entities/characters/Ken.js';
+import { FighterDirection } from './constants/fighter.js';
+
+export class FightingGame {
+    constructor() {
+        this.ctx = this.getContext();
+        this.fighters = [
+            new Ryu(104, STAGE_FLOOR, FighterDirection.RIGHT),
+            new Ken(280, STAGE_FLOOR, FighterDirection.LEFT),
+        ];
+
+        this.entities = [
+            new Stage(),
+            ...this.fighters,
+            new FpsCounter(),
+        ];
+
+        this.frameTime = {
+            previous: 0,
+            secondsPassed: 0,
+        };
+    }
+
+    getContext() {
+        const canvas = document.querySelector('canvas');
+        const ctx = canvas.getContext('2d');
+
+        ctx.imageSmoothingEnabled = false;
+        return ctx;
+    }
+
+    update() {
+        for (const entity of this.entities) {
+            entity.update(this.frameTime, this.ctx);
+        }
+    }
+
+    draw() {
+        for (const entity of this.entities) {
+            entity.draw(this.ctx);
+        }
+    }
+
+    // animation function
+    frame(time) {
+        window.requestAnimationFrame(this.frame.bind(this));
+
+        this.frameTime = {
+            secondsPassed: (time - this.frameTime.previous) / 1000,
+            previous: time,
+        }
+
+        this.update();
+        this.draw();
+    }
+
+    handleFormSubmit(e) {
+        e.preventDefault();
+
+        const selectedCheckboxes = Array
+        .from(e.target.querySelectorAll('input:checked'))
+        .map(checkbox => checkbox.value);
+
+        const options = e.target.querySelector('select');
+
+        this.fighters.forEach(fighter => {
+            if (selectedCheckboxes.includes(fighter.name)) {
+                fighter.changeState(options.value);
+            }
+        });
+    }
+
+    start() {
+        document.addEventListener('submit', this.handleFormSubmit.bind(this));
+
+        //requests animation at monitor's refresh rate
+        window.requestAnimationFrame(this.frame.bind(this));
+    }
+}
--- js/app.js
+++ js/app.js
@@ -1,11 +1,6 @@
 // import entity functions from entity modules
-import { Ryu } from './entities/characters/Ryu.js';
-import { Stage } from './entities/Background.js';
-import { Venom } from './entities/characters/Venom.js';
-import { FpsCounter } from './entities/FpsCounter.js';
-import { STAGE_FLOOR } from './constants/stage.js';
-import { Ken } from './entities/characters/Ken.js';
-import { FighterDirection, FighterState } from './constants/fighter.js';
+import { FightingGame } from './FightingGame.js';
+import { FighterState } from './constants/fighter.js';
 
 function populateMoveDropdown() {
     const dropdown = document.getElementById('state-dropdown');
@@ -18,66 +13,9 @@ function populateMoveDropdown() {
     });
 }
 
-function handleFormSubmit(e, fighters) {
-    e.preventDefault();
-
-    const selectedCheckboxes = Array.from(e.target.querySelectorAll('input:checked'))
-    .map(checkbox => checkbox.value);
-
-    const options = e.target.querySelector('select');
-
-    fighters.forEach(fighter => {
-       if (selectedCheckboxes.includes(fighter.name)) {
-        fighter.changeState(options.value);
-       }
-    });
-}
-
-
-// Ensure page loads before code runs
 window.addEventListener('load', function () {
     populateMoveDropdown();
-    const canvas = document.querySelector('canvas');
-    const ctx = canvas.getContext('2d');
 
-    ctx.imageSmoothingEnabled = false;
-
-    const fighters = [
-        new Ryu(104, STAGE_FLOOR, FighterDirection.RIGHT),
-        new Ken(280, STAGE_FLOOR, FighterDirection.LEFT),
-    ];
-
-    const entities = [
-        new Stage(),
-        ...fighters,
-        new FpsCounter(),
-    ];
-
-    let frameTime = {
-        previous: 0,
-        secondsPassed: 0,
-    };
-
-    // animation function
-    function frame(time) {
-        window.requestAnimationFrame(frame);
-
-        frameTime = {
-            secondsPassed: (time - frameTime.previous) / 1000,
-            previous: time,
-        }
-        for (const entity of entities) {
-            entity.update(frameTime, ctx);
-        }
-
-        for (const entity of entities) {
-            entity.draw(ctx);
-        }
-    }
-
-    this.document.addEventListener('submit', (e) => handleFormSubmit(e, fighters));
-
-    //requests animation at monitor's refresh rate
-    window.requestAnimationFrame(frame);
-}
-);
+    const fightingGame = new FightingGame();
+    fightingGame.start();
+});
