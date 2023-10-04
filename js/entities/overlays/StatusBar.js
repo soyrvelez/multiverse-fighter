dbdf7e8d32b582f99c95da01e5525d50dc1b861a
Rodrigo <velez59@gmail.com>
2023-10-04 08:00:45 -0700

    feat: added flashing animation to timer for last 14 seconds of fight

--- js/entities/overlays/StatusBar.js
+++ js/entities/overlays/StatusBar.js
@@ -1,9 +1,14 @@
+import { TIME_DELAY, TIME_FLASH_DELAY, TIME_FRAME_KEYS } from '../../constants/battle.js';
+
 export class StatusBar {
     constructor(fighters,) {
         this.image = document.querySelector('img[alt="game-ui"]');
 
         this.time = 99;
         this.timeTimer = 0;
+        this.timeFlashTimer = 0;
+        this.useFlashFrames = false;
+
         this.fighters = fighters;
 
         this.frames = new Map([
@@ -12,16 +17,28 @@ export class StatusBar {
             ['ko-white', [161, 16, 32, 14]],
 
             // Each of these represents a number used for the timer 0 - 9
-            ['time-0', [16, 32, 14, 16]],
-            ['time-1', [32, 32, 14, 16]],
-            ['time-2', [48, 32, 14, 16]],
-            ['time-3', [64, 32, 14, 16]],
-            ['time-4', [80, 32, 14, 16]],
-            ['time-5', [96, 32, 14, 16]],
-            ['time-6', [112, 32, 14, 16]],
-            ['time-7', [128, 32, 14, 16]],
-            ['time-8', [144, 32, 14, 16]],
-            ['time-9', [160, 32, 14, 16]],
+            [`${TIME_FRAME_KEYS[0]}-0`, [16, 32, 14, 16]],
+            [`${TIME_FRAME_KEYS[0]}-1`, [32, 32, 14, 16]],
+            [`${TIME_FRAME_KEYS[0]}-2`, [48, 32, 14, 16]],
+            [`${TIME_FRAME_KEYS[0]}-3`, [64, 32, 14, 16]],
+            [`${TIME_FRAME_KEYS[0]}-4`, [80, 32, 14, 16]],
+            [`${TIME_FRAME_KEYS[0]}-5`, [96, 32, 14, 16]],
+            [`${TIME_FRAME_KEYS[0]}-6`, [112, 32, 14, 16]],
+            [`${TIME_FRAME_KEYS[0]}-7`, [128, 32, 14, 16]],
+            [`${TIME_FRAME_KEYS[0]}-8`, [144, 32, 14, 16]],
+            [`${TIME_FRAME_KEYS[0]}-9`, [160, 32, 14, 16]],
+
+            // Frames used to flash timer numbers towards end of timer
+            [`${TIME_FRAME_KEYS[1]}-0`, [16, 192, 14, 16]],
+            [`${TIME_FRAME_KEYS[1]}-1`, [32, 192, 14, 16]],
+            [`${TIME_FRAME_KEYS[1]}-2`, [48, 192, 14, 16]],
+            [`${TIME_FRAME_KEYS[1]}-3`, [64, 192, 14, 16]],
+            [`${TIME_FRAME_KEYS[1]}-4`, [80, 192, 14, 16]],
+            [`${TIME_FRAME_KEYS[1]}-5`, [96, 192, 14, 16]],
+            [`${TIME_FRAME_KEYS[1]}-6`, [112, 192, 14, 16]],
+            [`${TIME_FRAME_KEYS[1]}-7`, [128, 192, 14, 16]],
+            [`${TIME_FRAME_KEYS[1]}-8`, [144, 192, 14, 16]],
+            [`${TIME_FRAME_KEYS[1]}-9`, [160, 192, 14, 16]],
 
             // Fighter game tags from the original game
             ['tag-ken', [128, 56, 30, 9]],
@@ -41,13 +58,48 @@ export class StatusBar {
         ctx.setTransform(1, 0, 0, 1, 0, 0);
     }
 
-    update(time, ctx) {
+    updateTime(time) {
+        if (time.previous > this.timeTimer + TIME_DELAY) {
+            this.time -= 1;
+            this.timeTimer = time.previous;
+        }
 
+        if (
+            this.time < 15 && this.time > -1
+            && time.previous > this.timeFlashTimer + TIME_FLASH_DELAY) {
+                this.useFlashFrames = !this.useFlashFrames;
+                this.timeFlashTimer = time.previous;
+        }
     }
 
-    draw(ctx) {
+    update(time) {
+        this.updateTime(time);
+    }
+
+    drawHealthBars(ctx) {
         this.drawFrame(ctx, 'health-bar', 31, 20);
         this.drawFrame(ctx, 'ko-white', 176, 18);
         this.drawFrame(ctx, 'health-bar', 353, 20, -1);
+    }
+
+    drawNameTags(ctx) {
+        const [{ name: name1}, { name: name2}] = this.fighters;
+
+        this.drawFrame(ctx, `tag-${name1.toLowerCase()}`, 32, 33);
+        this.drawFrame(ctx, `tag-${name2.toLowerCase()}`, 322, 33);
+    }
+
+    drawTime(ctx) {
+        const timeString = String(Math.max(this.time, 0)).padStart(2, '00');
+        const flashFrame = TIME_FRAME_KEYS[Number(this.useFlashFrames)];
+
+        this.drawFrame(ctx, `${flashFrame}-${timeString.charAt(0)}`, 178, 33);
+        this.drawFrame(ctx, `${flashFrame}-${timeString.charAt(1)}`, 194, 33);
+    }
+
+    draw(ctx) {
+        this.drawHealthBars(ctx);
+        this.drawNameTags(ctx);
+        this.drawTime(ctx);
     }
 }
