a2e1d81846a70c412055e02265df082f90b168bf
Rodrigo <velez59@gmail.com>
2023-10-04 09:43:36 -0700

    feat: replaced static bg for multi-layer background

--- js/entities/Background.js
+++ js/entities/Background.js
@@ -1,13 +1,31 @@
 export class Stage {
     constructor() {
-        this.image = document.querySelector('img[alt="background"]');
+        this.image = document.querySelector('img[alt="stage"]');
+
+        this.frames = new Map([
+            ['stage-background', [72, 208, 768, 176]],
+            ['stage-boat', [8, 16, 521, 180]],
+            ['stage-floor', [8, 392, 896, 72]],
+        ]);
     }
 
     update() {
 
     }
 
+    drawFrame(ctx, frameKey, x, y) {
+        const [sourceX, sourceY, sourceWidth, sourceHeight] = this.frames.get(frameKey);
+
+        ctx.drawImage(
+            this.image,
+            sourceX, sourceY, sourceWidth, sourceHeight,
+            x, y, sourceWidth, sourceHeight,
+        );
+    }
+
     draw(ctx) {
-        ctx.drawImage(background, 0, 0);
+        this.drawFrame(ctx, 'stage-background', -192, -16);
+        this.drawFrame(ctx, 'stage-boat', -128, -16);
+        this.drawFrame(ctx, 'stage-floor', -256, 160);
     }
 }
