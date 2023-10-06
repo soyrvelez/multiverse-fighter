ef7fae2c23c75e26e43dc82cf9e419eb1459c515
Rodrigo <velez59@gmail.com>
2023-10-06 16:43:23 -0700

    chore: added constructor draw and update functions to HitSplash.js

--- js/entities/characters/shared/HitSplash.js
+++ js/entities/characters/shared/HitSplash.js
@@ -1,3 +1,5 @@
+import { FRAME_TIME } from '../../../constants/game.js';
+
 export class HitSplash {
     constructor(x, y, playerId) {
         this.position = { x, y };
@@ -9,11 +11,24 @@ export class HitSplash {
         this.animationTimer = 0;
     }
 
-    update() {
-
+    update(time) {
+        if (time.previous < this.animationTimer + 4 * FRAME_TIME) return;
+        this.animationFrame += 1;
+        this.animationTimer = time.previous;
     }
 
-    draw() {
+    draw(ctx, camera) {
+        const [
+            [x, y, width, height], [originX, originY],
+        ] = this.frames[this.animationFrame + this.playerId * 4];
 
+        ctx.drawImage(
+            this.image,
+            x, y,
+            width, height,
+            Math.floor(this.position.x - camera.position.x - originX),
+            Math.floor(this.position.y - camera.position.y - originY),
+            width, height,
+        );
     }
 }
