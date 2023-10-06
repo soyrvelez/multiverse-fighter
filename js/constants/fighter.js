7bdff797b20809a57c4d4c611ad1e640b3cf5f6e
Rodrigo <velez59@gmail.com>
2023-10-05 18:33:03 -0700

    chore: added Fighter Attack Types to fighters constants file

--- js/constants/fighter.js
+++ js/constants/fighter.js
@@ -6,6 +6,11 @@ export const FighterDirection = {
     RIGHT: 1,
 };
 
+export const FighterAttackType = {
+    PUNCH: 'punch',
+    KICK: 'kick',
+}
+
 export const FighterState = {
     IDLE: 'idle',
     WALK_FORWARD: 'walkForwards',
